0cddb0a11f43a722baf89c4a77afb04eb4c7ae14
antonio martinez baez <tono.mtzb@gmail.com>
2020-10-02 10:32:27 -0500

    completed ex1, tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,29 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+
+def get_indices_of_item_weights(weights, length, limit):
+    check = {}
+
+    # creates check dict
+    for i, weight in enumerate(weights):
+        if weight not in check:
+            check[weight] =  i
+        else:
+             check[weight] =  i
+
+    for i, weight in enumerate(weights):
+        target = limit - weight
+        if target in check:
+            answer = [i, check[target]]
+            answer.sort(reverse=True)
+            answer = tuple(answer)
+            return answer
+    
     return None
+
+
+
+weights = [4, 4]
+length = 2
+limit = 8
+
+print(get_indices_of_item_weights(weights, length, limit))
